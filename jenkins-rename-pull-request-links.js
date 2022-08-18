3aac0f842564cc27e6be4b7dc7dbd8a8620865a6
Gregory Becker <igreg@users.noreply.github.com>
2022-08-18 15:09:21 +0100

    Update jenkins-rename-pull-request-links.js

--- jenkins-rename-pull-request-links.js
+++ jenkins-rename-pull-request-links.js
@@ -1,3 +1,14 @@
+// ==UserScript==
+// @name         Jenkins | rename pull-requests links
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  This script renames the links in the pull-requests view to show the PR title instead of the PR number
+// @author       Gregory Becker
+// @match        https://ci.shared.noths.com/*/view/change-requests/
+// @icon         https://ci.shared.noths.com/static/6f464c33/favicon.ico
+// @grant        none
+// ==/UserScript==
+
 if (window.location.pathname.match(/view\/change-requests/)) {
     $$('#projectstatus a.model-link').each(function(pr_link) {
         console.log("hello world")
